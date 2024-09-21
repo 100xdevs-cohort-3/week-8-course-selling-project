baa33ddd2f1d97b5eb46b925f29867441a04f543
harkirat <harkirat96@gmail.com>
2024-09-21 20:43:25 +0530

    Step 4 , added course router

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const { userRouter } = require("./routes/user");
-const { createCourseRoutes } = require("./routes/course");
+const { courseRouter } = require("./routes/course");
 const app = express();
 
-app.use("/user", userRouter);
-app.use("/course", courseRouter);
-
-createUserRoutes(app);
-createCourseRoutes(app);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
 app.listen(3000);
--- routes/course.js
+++ routes/course.js
@@ -1,20 +1,19 @@
+const { Router } = require("express");
+const courseRouter = Router();
 
-function createCourseRoutes(app) {
-
-    app.post("/course/purchase", function(req, res) {
-        // you would expect the user to pay you money
-        res.json({
-            message: "signup endpoint"
-        })
+courseRouter.post("/purchase", function(req, res) {
+    // you would expect the user to pay you money
+    res.json({
+        message: "signup endpoint"
     })
+})
 
-    app.get("/course/preview", function(req, res) {
-        res.json({
-            message: "signup endpoint"
-        })
+courseRouter.get("/preview", function(req, res) {
+    res.json({
+        message: "course preview endpoint"
     })
-}
+})
 
 module.exports = {
-    createCourseRoutes: createCourseRoutes
+    courseRouter: courseRouter
 }
