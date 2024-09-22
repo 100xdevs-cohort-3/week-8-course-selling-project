4031d3bb0cd3897607f9d5d461def8db2caa5f7c
harkirat <harkirat96@gmail.com>
2024-09-22 21:21:07 +0530

    Added admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -70,15 +70,38 @@ adminRouter.post("/course", adminMiddleware, async function(req, res) {
     })
 })
 
-adminRouter.put("/course", function(req, res) {
+adminRouter.put("/course", adminMiddleware, async function(req, res) {
+    const adminId = req.userId;
+
+    const { title, description, imageUrl, price, courseId } = req.body;
+
+    // creating a web3 saas in 6 hours
+    const course = await courseModel.updateOne({
+        _id: courseId, 
+        creatorId: adminId 
+    }, {
+        title: title, 
+        description: description, 
+        imageUrl: imageUrl, 
+        price: price
+    })
+
     res.json({
-        message: "signup endpoint"
+        message: "Course updated",
+        courseId: course._id
     })
 })
 
-adminRouter.get("/course/bulk", function(req, res) {
+adminRouter.get("/course/bulk", adminMiddleware,async function(req, res) {
+    const adminId = req.userId;
+
+    const courses = await courseModel.find({
+        creatorId: adminId 
+    });
+
     res.json({
-        message: "signup endpoint"
+        message: "Course updated",
+        courses
     })
 })
 
